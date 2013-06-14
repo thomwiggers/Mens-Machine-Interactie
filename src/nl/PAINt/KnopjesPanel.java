57962198e68ea031111dd8515b0d19c04b7c415f
Thom Wiggers <thom@thomwiggers.nl>
2013-06-14 22:24:34 +0200

    operaties

--- src/nl/PAINt/KnopjesPanel.java
+++ src/nl/PAINt/KnopjesPanel.java
@@ -12,6 +12,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -20,10 +22,10 @@ import javax.swing.JPanel;
  * @author Luuk Scholten & Thom Wiggers
  * 
  */
-public class KnopjesPanel extends JPanel {
+public class KnopjesPanel extends JPanel implements ActionListener {
 
     public enum KnopjesActies {
-	VIERKANT_TEKENEN, VIERKANT_TEKENEN_FILLED, ELLIPS_TEKENEN, ELLIPSE_TEKENEN_FILLED, DRIEHOEK_TEKENEN, DRIEHOEK_TEKENEN_FILLED, MOVE, DELETE;
+	VIERKANT_TEKENEN, VIERKANT_TEKENEN_FILLED, ELLIPS_TEKENEN, ELLIPSE_TEKENEN_FILLED, DRIEHOEK_TEKENEN, DRIEHOEK_TEKENEN_FILLED, RESIZE, DELETE;
 
     }
 
@@ -49,12 +51,14 @@ public class KnopjesPanel extends JPanel {
 
     private void initKnopjes() {
 	JButton vierkantKnopje = new JButton("Sq");
+	vierkantKnopje.addActionListener(this);
 	vierkantKnopje.setActionCommand(KnopjesActies.VIERKANT_TEKENEN
 		.toString());
 	vierkantKnopje.setSize(new Dimension(40, 40));
 	vierkantKnopje.setMargin(new Insets(0, 0, 0, 0));
 
 	JButton vierkantKnopjeFilled = new JButton("SqF");
+	vierkantKnopjeFilled.addActionListener(this);
 	vierkantKnopjeFilled
 		.setActionCommand(KnopjesActies.VIERKANT_TEKENEN_FILLED
 			.toString());
@@ -62,11 +66,13 @@ public class KnopjesPanel extends JPanel {
 	vierkantKnopjeFilled.setMargin(new Insets(0, 0, 0, 0));
 
 	JButton ellipseKnopje = new JButton("El");
+	ellipseKnopje.addActionListener(this);
 	ellipseKnopje.setActionCommand(KnopjesActies.ELLIPS_TEKENEN.toString());
 	ellipseKnopje.setSize(new Dimension(40, 40));
 	ellipseKnopje.setMargin(new Insets(0, 0, 0, 0));
 
 	JButton ellipseKnopjeFilled = new JButton("ElF");
+	ellipseKnopjeFilled.addActionListener(this);
 	ellipseKnopjeFilled
 		.setActionCommand(KnopjesActies.ELLIPSE_TEKENEN_FILLED
 			.toString());
@@ -74,23 +80,29 @@ public class KnopjesPanel extends JPanel {
 	ellipseKnopjeFilled.setMargin(new Insets(0, 0, 0, 0));
 
 	JButton driehoekKnopje = new JButton("Tr");
+	driehoekKnopje.addActionListener(this);
 	driehoekKnopje.setActionCommand(KnopjesActies.DRIEHOEK_TEKENEN
 		.toString());
 	driehoekKnopje.setSize(new Dimension(40, 40));
 	driehoekKnopje.setMargin(new Insets(0, 0, 0, 0));
 
-	JButton driehoekKnopjeFilled = new JButton("Tr");
+	JButton driehoekKnopjeFilled = new JButton("TrF");
+	driehoekKnopjeFilled.addActionListener(this);
 	driehoekKnopjeFilled
 		.setActionCommand(KnopjesActies.DRIEHOEK_TEKENEN_FILLED
 		.toString());
 	driehoekKnopjeFilled.setSize(new Dimension(40, 40));
 	driehoekKnopjeFilled.setMargin(new Insets(0, 0, 0, 0));
 
-	JButton moveKnopje = new JButton("Mv");
+	JButton moveKnopje = new JButton("Re");
+	moveKnopje.setActionCommand(KnopjesActies.RESIZE.toString());
+	moveKnopje.addActionListener(this);
 	moveKnopje.setSize(40, 40);
 	moveKnopje.setMargin(new Insets(0, 0, 0, 0));
 
 	JButton deleteKnopje = new JButton("X");
+	deleteKnopje.setActionCommand(KnopjesActies.DELETE.toString());
+	deleteKnopje.addActionListener(this);
 	deleteKnopje.setSize(40, 40);
 	deleteKnopje.setMargin(new Insets(0, 0, 0, 0));
 
@@ -102,6 +114,46 @@ public class KnopjesPanel extends JPanel {
 	this.add(driehoekKnopjeFilled);
 	this.add(moveKnopje);
 	this.add(deleteKnopje);
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+	switch (KnopjesActies.valueOf(e.getActionCommand())) {
+	case DELETE:
+	    canvas.setMode(PanelMode.DELETE);
+	    break;
+	case DRIEHOEK_TEKENEN:
+	    canvas.setMode(PanelMode.TRIANGLE);
+	    break;
+	case DRIEHOEK_TEKENEN_FILLED:
+	    canvas.setMode(PanelMode.TRIANGLE);
+	    break;
+	case ELLIPSE_TEKENEN_FILLED:
+	    canvas.setMode(PanelMode.ELL_FILLED);
+	    break;
+	case ELLIPS_TEKENEN:
+	    canvas.setMode(PanelMode.ELL);
+	    break;
+	case RESIZE:
+	    canvas.setMode(PanelMode.RESIZE);
+	    break;
+	case VIERKANT_TEKENEN:
+	    canvas.setMode(PanelMode.RECT);
+	    break;
+	case VIERKANT_TEKENEN_FILLED:
+	    canvas.setMode(PanelMode.RECT);
+	    break;
+	default:
+	    break;
+
+	}
 
     }
 
