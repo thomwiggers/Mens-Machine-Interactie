8469dde925dbdd542e4dbe26eb7d55c756004ab0
Thom Wiggers <thom@thomwiggers.nl>
2013-06-14 21:02:01 +0200

    meer knopjes

--- src/nl/PAINt/KnopjesPanel.java
+++ src/nl/PAINt/KnopjesPanel.java
@@ -10,7 +10,10 @@ package nl.PAINt;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Insets;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 /**
@@ -18,6 +21,11 @@ import javax.swing.JPanel;
  * 
  */
 public class KnopjesPanel extends JPanel {
+
+    public enum KnopjesActies {
+	VIERKANT_TEKENEN, VIERKANT_TEKENEN_FILLED, ELLIPS_TEKENEN, ELLIPSE_TEKENEN_FILLED, DRIEHOEK_TEKENEN, DRIEHOEK_TEKENEN_FILLED, MOVE, DELETE;
+
+    }
 
     /**
      * 
@@ -28,13 +36,70 @@ public class KnopjesPanel extends JPanel {
      * constructor doet dingen
      */
     public KnopjesPanel() {
-	this.setPreferredSize(new Dimension(40, 900));
+	this.setPreferredSize(new Dimension(80, 90));
+	this.setLayout(new GridLayout(0, 2));
 	setBackground(Color.blue);
 	setAlignmentX(LEFT_ALIGNMENT);
 
-	this.setVisible(true);
+	initKnopjes();
+    }
 
-	// todo knopjes toevoegen
+    private void initKnopjes() {
+	JButton vierkantKnopje = new JButton("Sq");
+	vierkantKnopje.setActionCommand(KnopjesActies.VIERKANT_TEKENEN
+		.toString());
+	vierkantKnopje.setSize(new Dimension(40, 40));
+	vierkantKnopje.setMargin(new Insets(0, 0, 0, 0));
+
+	JButton vierkantKnopjeFilled = new JButton("SqF");
+	vierkantKnopjeFilled
+		.setActionCommand(KnopjesActies.VIERKANT_TEKENEN_FILLED
+			.toString());
+	vierkantKnopjeFilled.setSize(new Dimension(40, 40));
+	vierkantKnopjeFilled.setMargin(new Insets(0, 0, 0, 0));
+
+	JButton ellipseKnopje = new JButton("El");
+	ellipseKnopje.setActionCommand(KnopjesActies.ELLIPS_TEKENEN.toString());
+	ellipseKnopje.setSize(new Dimension(40, 40));
+	ellipseKnopje.setMargin(new Insets(0, 0, 0, 0));
+
+	JButton ellipseKnopjeFilled = new JButton("ElF");
+	ellipseKnopjeFilled
+		.setActionCommand(KnopjesActies.ELLIPSE_TEKENEN_FILLED
+			.toString());
+	ellipseKnopjeFilled.setSize(new Dimension(40, 40));
+	ellipseKnopjeFilled.setMargin(new Insets(0, 0, 0, 0));
+
+	JButton driehoekKnopje = new JButton("Tr");
+	driehoekKnopje.setActionCommand(KnopjesActies.DRIEHOEK_TEKENEN
+		.toString());
+	driehoekKnopje.setSize(new Dimension(40, 40));
+	driehoekKnopje.setMargin(new Insets(0, 0, 0, 0));
+
+	JButton driehoekKnopjeFilled = new JButton("Tr");
+	driehoekKnopjeFilled
+		.setActionCommand(KnopjesActies.DRIEHOEK_TEKENEN_FILLED
+		.toString());
+	driehoekKnopjeFilled.setSize(new Dimension(40, 40));
+	driehoekKnopjeFilled.setMargin(new Insets(0, 0, 0, 0));
+
+	JButton moveKnopje = new JButton("Mv");
+	moveKnopje.setSize(40, 40);
+	moveKnopje.setMargin(new Insets(0, 0, 0, 0));
+
+	JButton deleteKnopje = new JButton("X");
+	deleteKnopje.setSize(40, 40);
+	deleteKnopje.setMargin(new Insets(0, 0, 0, 0));
+
+	this.add(vierkantKnopje);
+	this.add(vierkantKnopjeFilled);
+	this.add(ellipseKnopje);
+	this.add(ellipseKnopjeFilled);
+	this.add(driehoekKnopje);
+	this.add(driehoekKnopjeFilled);
+	this.add(moveKnopje);
+	this.add(deleteKnopje);
+
     }
 
 }
